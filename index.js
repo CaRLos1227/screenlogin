ad7bc6f3a566d30665ac5b1fce480295957215fd
CARLOS..- <96316594+CaRLos1227@users.noreply.github.com>
2022-07-06 18:42:54 -0300

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const http = require('http');
 const { readFileSync, writeFileSync } = require('fs');
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const db = JSON.parse(readFileSync('./db/users.json'));
 
 app.get('/', (req, res) => {
